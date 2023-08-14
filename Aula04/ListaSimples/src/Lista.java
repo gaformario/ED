327d3d770e78fbed3a1e725550958cb245f1a470
gaformario <gabriel.formario@hotmail.com>
2023-08-14 14:47:53 -0300

    Aula Lista Simples

--- Aula04/ListaSimples/src/Lista.java
+++ Aula04/ListaSimples/src/Lista.java
@@ -3,8 +3,9 @@ public class Lista {
     No inicio, fim;
     int tamanho;
 
-    public void inserir(int idade) {
+    // método de inserção de elemento numa lista simples
 
+    public void inserir(int idade) { 
         No aux = new No();
         aux.idade = idade;
 
@@ -20,6 +21,18 @@ public class Lista {
 
         fim = aux;
         tamanho++;
+
+    }
+
+    // método de impressão de elementos de uma lista
+    public void imprimir() {
+        No aux = inicio;
+
+        while (aux != null) {
+            
+            System.out.println(aux.idade);
+            aux = aux.prox;
+        }
 
     }
 }
---
+++ Aula04/ListaSimples/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.inserir(45);
+        lista.inserir(20);
+
+        lista.imprimir();
+    }
+}
